2fc8fe7ee888ab9f73f5d15679b9a4d5331f1a70
Ilya Tychinin <elive@mail.ru>
2012-10-29 18:24:18 +0600

    переименовал переменные

--- dz1.js
+++ dz1.js
@@ -1,30 +1,32 @@
-/*jslint browser: true*/
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+﻿/*jslint browser: true*/
+var arrayForSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function sRand() {
     'use strict';
     return Math.random() > 0.5 ? 1 : -1;
 }
 
-function sBubble(arr) {
+function sBubble(arrayForSort) {
     'use strict';
     var i, j, tmp;
-    for (i = 0; i < arr.length - 1; i += 1) {
-        for (j = 0; j < arr.length - 1; j += 1) {
-            if (arr[j + 1] < arr[j]) {
-                tmp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = tmp;
+    for (i = 0; i < arrayForSort.length - 1; i += 1) {
+        for (j = 0; j < arrayForSort.length - 1; j += 1) {
+            if (arrayForSort[j + 1] < arrayForSort[j]) {
+                tmp = arrayForSort[j + 1];
+                arrayForSort[j + 1] = arrayForSort[j];
+                arrayForSort[j] = tmp;
             }
         }
     }
-    return arr;
+    return arrayForSort;
 }
 
-arr.sort(sRand);
+arrayForSort.sort(sRand);
 
-window.onload = (function () {
+window.addEventListener('load', function () {
     'use strict';
-    document.getElementById('arr').innerHTML = arr;
-    document.getElementById('btn').onclick = function () { document.getElementById('sarr').innerHTML = sBubble(arr); };
-}());
+    document.getElementById('arrayForSort').innerHTML = arrayForSort;
+    document.getElementById('sortButton').addEventListener('click', function () {
+        document.getElementById('sortResult').innerHTML = sBubble(arrayForSort);
+    }, false);
+}, false);
